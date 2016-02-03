bace0977dcc6fb71d45e4ea92874373de11c2e3a
Sanjana <navyasanjana4@gmail.com>
2016-02-03 13:58:29 -0600

    movie same

--- js/app.js
+++ js/app.js
@@ -1,30 +1,90 @@
 'use strict';
+angular.module('myapp', ['ngRoute'])
+    .config(['$routeProvider',
+        function($routeProvider, $locationProvider) {
+            $routeProvider
+                .when('/song', {
+                    templateUrl: 'song.html'
+                })
+                .when('/movie', {
+                    templateUrl: 'movie.html'
+                });
 
-angular.module('myApp', [])
-    .controller('MovieController', function($scope, $http) {
-      $scope.$watch('search', function() {
-        fetch();
-      });
 
-      $scope.search = "";
+        }])
+        .controller('MovieController', function($routeParams,$scope, $http) {
+        this.params=$routeParams;
+        $scope.$watch('search', function() {
+            fetch();
+        });
 
-      function fetch() {
-        $http.get("http://www.omdbapi.com/?t=" + $scope.search + "&tomatoes=true&plot=full")
-            .then(function(response) {
-              $scope.details = response.data;
+        $scope.search = "";
+
+        function fetch() {
+            $http.get("http://www.omdbapi.com/?t=" + $scope.search)
+                .then(function(response) {
+                    $scope.details = response.data;
+                });
+
+            $http.get("http://www.omdbapi.com/?s=" + $scope.search)
+                .then(function(response) {
+                    $scope.related = response.data;
+                });
+        }
+
+            $scope.update = function(movie) {
+            $scope.search = movie.Title;
+        };
+
+            $scope.select = function() {
+            this.setSelectionRange(0, this.value.length);
+        }
+    })
+    .factory("httpService",function($http) {
+
+    return {
+        getalbums: function () {
+            var album = "https://freemusicarchive.org/api/get/albums.json?api_key=4CEU5MLWCWUU0RWJ"
+            return $http.get(album).then(function (response) {
+                return response.data;
             });
-
-        $http.get("http://www.omdbapi.com/?s=" + $scope.search)
-            .then(function(response) {
-              $scope.related = response.data;
+        },
+        gettracks: function (gettrack) {
+            var track = "https://freemusicarchive.org/api/get/tracks.json?api_key=4CEU5MLWCWUU0RWJ&album_id="+gettrack;
+            return $http.get(track).then(function (response) {
+                return response.data;
             });
-      }
+        },
+        getartists: function (getartists) {
+            var artist = "https://freemusicarchive.org/api/get/artists.json?api_key=4CEU5MLWCWUU0RWJ&artist_id="+getartists;
+            return $http.get(artist).then(function (response) {
+                return response.data;
+            });
+        }
+    }
+})
+    .controller("songcntrl",function(httpService,$scope,$http,$routeParams) {
+        this.params=$routeParams;
+    $scope.data1={};
+    $scope.data2={};
+    httpService.getalbums().then(function(response){
+        $scope.data1=response;
 
-      $scope.update = function(movie) {
-        $scope.search = movie.Title;
-      };
-
-      $scope.select = function() {
-        this.setSelectionRange(0, this.value.length);
-      }
     });
+    console.log($scope.selection);
+    $scope.call=function(val){
+        httpService.gettracks(val).then(function(response){
+            $scope.data2=response;
+            console.log(response);
+
+        });
+        $scope.getArtist=function(id){
+            httpService.getartists(id).then(function(response){
+                console.log(response);
+                alert(response.dataset.artist_bio);
+            });
+        }
+    }
+
+
+});
